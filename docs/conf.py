d31c8f90489e3c8ed319844727d4d9f313ab6034
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-01-18 15:41:40 -0600

    ci: add python >3.6 support
    Add support for Python version 3.6 and higher.  Prepare to tag 0.4.1.
    
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2021, Jeremy A Gray"
 author = "Jeremy A Gray"
 
 # The full version, including alpha/beta/rc tags
-release = "0.4.0"
+release = "0.4.1"
 
 
 # -- General configuration ---------------------------------------------------
