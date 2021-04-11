19cc130917d92570198573917110de7d97d9d515
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-04-11 10:31:42 -0500

    release: prepare to tag 0.4.3
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2021, Jeremy A Gray"
 author = "Jeremy A Gray"
 
 # The full version, including alpha/beta/rc tags
-release = "0.4.2"
+release = "0.4.3"
 
 
 # -- General configuration ---------------------------------------------------
