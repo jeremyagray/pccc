92311854130bc4b851ef7f11ad1a21e073fc62a7
Jeremy A Gray <jeremy.a.gray@gmail.com>
2022-03-23 10:38:46 -0500

    fix(tooling): add sphinx path to local variables
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,6 @@
 
 ((python-mode
   (flycheck-python-flake8-executable . "/home/gray/.virtualenvs/pccc/bin/flake8")
-  (flycheck-python-pylint-executable . "/home/gray/.virtualenvs/pccc/bin/flake8")))
+  (flycheck-python-pylint-executable . "/home/gray/.virtualenvs/pccc/bin/flake8"))
+ (rst-mode
+  (flycheck-rst-sphinx-executable . "/home/gray/.virtualenvs/pccc/bin/sphinx-build")))
